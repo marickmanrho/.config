ddc1660f4bab2215249c2ba077229e9bb036075f
Marick Manrho <marickmanrho@gmail.com>
2026-01-23 15:58:08 +0100

    Create links to zshrc files and add Pure

--- zsh/install_zsh.sh
+++ zsh/install_zsh.sh
@@ -1,5 +1,15 @@
 # Install zsh
 
+echo "Installing zsh"
 sudo apt-get install zsh
 
+echo "Making zsh default shell"
 chsh -s /usr/bin/zsh
+
+echo "Making dynamic links to config files"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ln -s ${BASEDIR}/zshrc.file ~/.zshrc
+
+mkdir -p "${BASEDIR}/zsh"
+ln -s ${BASEDIR} ~/.zsh
+
