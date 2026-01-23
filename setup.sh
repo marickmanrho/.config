f8ba5fed356cf2d6506bd500b910abdf05c3f6a7
Marick Manrho <marickmanrho@gmail.com>
2026-01-23 15:40:45 +0100

    Install zsh and use as default

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+
+echo "Install zsh?"
+select zsh_yn in "Yes" "No"; do
+	case $zsh_yn in
+		Yes ) bash ./zsh/install_zsh.sh; break;;
+		No ) echo "Skipping"; break;;
+	esac
+done
+
+
 echo "Install NeoVim?"
-select yn in "Yes" "No"; do
- 	case $yn in
+select nv_yn in "Yes" "No"; do
+ 	case $nv_yn in
  		Yes ) bash ./install_scripts/install_nvim.sh; break;;
  		No ) echo "Skipping"; break;;
 	esac
---
+++ zsh/install_zsh.sh
@@ -0,0 +1,5 @@
+# Install zsh
+
+sudo apt-get install zsh
+
+chsh -s /usr/bin/zsh
